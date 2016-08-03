b95de7a7a1f1f6bbf9f4e2a4ad6a8575a5f9f0d9
Cesare Ferrari <cesare@maryandferrari.com>
2016-08-03 08:31:41 -0400

    Add exercises lesson 3

---
+++ lesson-3/ex5_1.rb
@@ -0,0 +1,14 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" }
+}
+
+male_ages = 0
+
+munsters.each do |name, details|
+  male_ages += details['age'] if details['gender'] == 'male'
+end
+
+puts male_ages
