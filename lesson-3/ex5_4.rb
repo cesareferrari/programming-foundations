8ea17c34fa1814f6720281303a06cd96a467e2f1
Cesare Ferrari <cesare@maryandferrari.com>
2016-08-03 09:20:26 -0400

    Add lesson 3 exercises

---
+++ lesson-3/ex5_4.rb
@@ -0,0 +1,10 @@
+sentence = "Humpty Dumpty sat on a wall."
+
+puts sentence.split.reverse.join(' ')
+
+
+words = sentence.split(/\W/)
+words.reverse!
+backwards_sentence = words.join(' ') + '.'
+
+puts backwards_sentence
