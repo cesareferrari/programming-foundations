aaaf55a79a705766e97af5e6b62d1d547cd628d8
Cesare Ferrari <cesare@maryandferrari.com>
2016-08-08 09:34:11 -0400

    Add exercise

--- lesson-3/ex7_4.rb
+++ lesson-3/ex7_4.rb
@@ -1,9 +1,20 @@
-require 'securerandom'
-include SecureRandom
+# "f65c57f6-a6aa-17a8-faa1-a67f2dc9fa91"
 
-def generate
-  SecureRandom.hex(32)
+def generate_uuid
+  uuid = []
+  pool = (0..9).to_a + ('a'..'f').to_a
+  section_sizes = [8, 4, 4, 4, 12]
+
+  section_sizes.each do |size|
+    size.times { uuid << pool.sample }
+    uuid << '-'
+  end
+
+  uuid.pop # remove last '-'
+  uuid.join # convert to string
 end
 
-puts generate
-puts generate
+puts generate_uuid
+puts generate_uuid
+puts generate_uuid
+puts generate_uuid
