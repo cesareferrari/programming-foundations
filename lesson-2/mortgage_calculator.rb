4317a6231735e2eba17872e7cfbaff9e4e92a251
Cesare Ferrari <cesare@maryandferrari.com>
2016-11-01 09:29:23 -0400

    Add mortgage calculator

--- lesson-2/mortgage_calculator.rb
+++ lesson-2/mortgage_calculator.rb
@@ -1,5 +1,13 @@
-def prompt(message)
-  puts "==> #{message}"
+require 'yaml'
+
+MESSAGES = YAML.load_file('mortgage_calculator_messages.yaml')
+
+def prompt(message, extra = '')
+  if MESSAGES[message]
+    puts "==> #{MESSAGES[message]} #{extra}"
+  else
+    puts "==> #{message}"
+  end
 end
 
 def valid_float?(value)
@@ -10,46 +18,46 @@ def valid_integer?(value)
   value.to_i > 0
 end
 
-prompt "Welcome to Mortgage Calculator"
+prompt 'welcome'
 
 loop do
   amount = ''
   loop do
-    prompt "Enter the loan amount in dollars:"
+    prompt 'enter_loan_amount'
     amount = gets.chomp
     break if valid_float?(amount)
-    prompt "Please, enter a valid amount (example: '12000', '870.75')."
+    prompt 'valid_amount'
   end
 
   yearly_interest_rate = ''
   loop do
-    prompt "Enter the yearly interest rate (APR):"
+    prompt 'enter_apr'
     yearly_interest_rate = gets.chomp
     break if valid_float?(yearly_interest_rate)
-    prompt "Please, enter a valid interest rate (example: '2.5' for '2.5%')."
+    prompt 'valid_interest'
   end
 
   duration_years = ''
   loop do
-    prompt "Enter the loan duration in years"
+    prompt 'enter_duration'
     duration_years = gets.chomp
 
     break if valid_integer?(duration_years)
-    prompt "Please, enter a valid duration period in years (example: '2', '10')."
+    prompt 'valid_duration'
   end
 
   duration_months = duration_years.to_i * 12
   monthly_interest_rate = yearly_interest_rate.to_f / 100 / 12
   monthly_payment = amount.to_i * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**-duration_months))
 
-  prompt "Loan amount: #{amount}"
-  prompt "Yearly interest rate (APR): #{yearly_interest_rate}"
-  prompt "Duration Years: #{duration_years} (#{duration_months} months)"
-  prompt "Your monthly payment is: #{format('%.2f', monthly_payment)}"
+  prompt('amount', amount)
+  prompt('apr', yearly_interest_rate)
+  prompt "Duration Years: #{duration_years.to_i} (#{duration_months} months)"
+  prompt('payment', format('%.2f', monthly_payment))
 
-  prompt "Do you want to perform another calculation? (Y/N)"
+  prompt 'perform_another'
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt "Thank you for using Mortgage Calculator"
+prompt 'thanks'
