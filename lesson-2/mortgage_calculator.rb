cce2be0a8176b960075e1185ca2367575708b653
Cesare Ferrari <cesare@maryandferrari.com>
2016-11-05 09:45:17 -0400

    Mortgage calculator corrections

--- lesson-2/mortgage_calculator.rb
+++ lesson-2/mortgage_calculator.rb
@@ -6,11 +6,8 @@ def prompt(message, *args)
   puts "==> #{format(MESSAGES[message], *args)}"
 end
 
-def valid_float?(value)
-  value.to_f > 0
-end
-
-def valid_integer?(value)
+def valid_input?(value, can_be_a_float = true)
+  return value.to_f > 0 if can_be_a_float
   value.to_i > 0
 end
 
@@ -20,34 +17,36 @@ name = gets.chomp
 name = %w(Stranger Honey Buddy).sample if name.empty?
 
 loop do
-  amount = ''
+  amount = nil
   loop do
     prompt('enter_loan_amount', name)
     amount = gets.chomp
-    break if valid_float?(amount)
+    break if valid_input?(amount)
     prompt 'valid_amount'
   end
 
-  yearly_interest_rate = ''
+  yearly_interest_rate = nil
   loop do
     prompt 'enter_apr'
     yearly_interest_rate = gets.chomp
-    break if valid_float?(yearly_interest_rate)
+    break if valid_input?(yearly_interest_rate)
     prompt 'valid_interest'
   end
 
-  duration_years = ''
+  duration_years = nil
   loop do
     prompt 'enter_duration'
     duration_years = gets.chomp
 
-    break if valid_integer?(duration_years)
+    break if valid_input?(duration_years, false)
     prompt 'valid_duration'
   end
 
   duration_months = duration_years.to_i * 12
   monthly_interest_rate = yearly_interest_rate.to_f / 100 / 12
-  monthly_payment = amount.to_i * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**-duration_months))
+  monthly_payment =
+    amount.to_i *
+    (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**-duration_months))
 
   prompt('amount', amount)
   prompt('apr', yearly_interest_rate)
